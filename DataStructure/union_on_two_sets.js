416589306ec4889615c994da8f023808b7216f6e
linhletd <linhletd@gmail.com>
2020-02-06 23:56:17 +0700

    complete union_on_two_sets but not passing

---
+++ DataStructure/union_on_two_sets.js
@@ -0,0 +1,49 @@
+function Set() {
+    // the var collection will hold the set
+    var collection = [];
+    // this method will check for the presence of an element and return true or false
+    this.has = function(element) {
+        return (collection.indexOf(element) !== -1);
+    };
+    // this method will return all the values in the set
+    this.values = function() {
+        return collection;
+    };
+    // this method will add an element to the set
+    this.add = function(element) {
+        if(!this.has(element)){
+            collection.push(element);
+            return true;
+        }
+        return false;
+    };
+   // this method will remove an element from a set
+    this.remove = function(element) {
+        if(this.has(element)){
+           var index = collection.indexOf(element);
+            collection.splice(index,1);
+            return true;
+        }
+        return false;
+    };
+    // this method will return the size of the set
+    this.size = function() {
+        return collection.length;
+    };
+    // change code below this line
+    this.union = function(set){
+        let newSet = new Set();
+        this.values().map(cur => {newSet.add(cur)});
+        set.map(cur => {newSet.add(cur);})
+        return newSet.values();
+        
+    }
+    // change code above this line
+}
+
+let obj = new Set();
+obj.add('a');
+obj.add('b');
+obj.add('c');
+
+console.log(obj.values(),obj.union(['c','d']));
