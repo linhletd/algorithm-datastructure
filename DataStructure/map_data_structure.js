f3b0a1434e28800472ba4924806231033abd4f0d
linhletd <linhletd@gmail.com>
2020-02-07 23:42:40 +0700

    add entries method

--- DataStructure/map_data_structure.js
+++ DataStructure/map_data_structure.js
@@ -1,11 +1,18 @@
 var Map = function() {
     this.collection = {};
+    this._entries = [];
     // change code below this line
+    this.entries = function(){
+        return this._entries;
+    }
     this.add = function(key, value){
       this.collection[key] = value;
+      this._entries.push(key);
     };
     this.remove = function(key){
       delete this.collection[key];
+      let removeIndex = this.entries().indexOf(key);
+      this._entries.splice(removeIndex, 1);
     };
     this.has = function(key){
       return this.collection.hasOwnProperty(key);
@@ -21,10 +28,12 @@ var Map = function() {
     };
     this.clear = function(){
       this.collection = {};
+      this._entries = [];
     };
     this.size = function(){
       return Object.keys(this.collection).length
     }
+    
     // change code above this line
   };
   
